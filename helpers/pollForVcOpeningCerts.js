c12dd770b40330c1df8d8608e9ff15cc1e0a2491
Rahul <rksethuram9@gmail.com>
2018-05-17 12:49:54 -0700

    Recover certs for each channel member

--- helpers/pollForVcOpeningCerts.js
+++ helpers/pollForVcOpeningCerts.js
@@ -1,4 +1,5 @@
 const { getModels } = require('../models')
+const Ethcalate = require('../../ethcalate-testing/src/src')
 const { getEthcalate } = require('../web3')
 
 const TOTAL_POLLING_TIME = 3600 * 1000
@@ -23,27 +24,59 @@ module.exports = async virtualChannel => {
         virtualchannelId: id
       }
     })
+    const { agentA, agentB, ingrid, depositA, depositB } = virtualChannel
     // recover signer and make sure both agents submitted signed certs
     certs.forEach(cert => {
       console.log(`Found opening certs from ${cert.from}`)
-      const signer = ethcalate.recoverSignerFromOpeningCerts(
-        cert.sig,
-        virtualChannel
-      )
-      switch (signer) {
+      let signer
+      switch (cert.from) {
         case agentA:
+          signer = Ethcalate.recoverSignerFromOpeningCerts(cert.sig, {
+            id,
+            agentA,
+            agentB,
+            ingrid,
+            participantType: 'agentA',
+            depositInWei: depositA
+          })
+          if (signer === agentA) {
+            found[signer] = true
+          } else {
+            console.log(`Invalid cert signature: ${cert.id}`)
+          }
+          break
         case agentB:
-          found[signer] = true
+          signer = Ethcalate.recoverSignerFromOpeningCerts(cert.sig, {
+            id,
+            agentA,
+            agentB,
+            ingrid,
+            participantType: 'agentB',
+            depositInWei: depositB
+          })
+          if (signer === agentB) {
+            found[signer] = true
+          } else {
+            console.log(`Invalid cert signature: ${cert.id}`)
+          }
           break
       }
     })
     if (found[agentA] && found[agentB]) {
-      const certs = await ethcalate.createOpeningCerts(virtualChannel, true)
+      const certs = await ethcalate.createOpeningCerts(
+        {
+          id,
+          agentA,
+          agentB,
+          ingrid,
+          depositInWei: '0',
+          participantType: 'ingrid'
+        },
+        true
+      )
       console.log('Ingrid created certs after finding other certs: ', certs)
-
-      await ethcalate.sendOpeningCerts(id, certs)
-
       clearInterval(intervalId)
+      await ethcalate.sendOpeningCerts(id, certs)
     }
     if (counter >= NUM_POLLING_INTERVALS) {
       console.log('Could not find opening certs within total polling time.')
