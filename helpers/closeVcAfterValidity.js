2ea3e2e5ab12a33512d0c0bfa07f89a7ef3f013a
Rahul <rksethuram9@gmail.com>
2018-05-21 21:12:47 -0700

    Decompose to ledger function

--- helpers/closeVcAfterValidity.js
+++ helpers/closeVcAfterValidity.js
@@ -1,19 +1,12 @@
-const { getEthcalate, getWeb3 } = require('../web3')
+const { getEthcalate } = require('../web3')
+const decomposeToLedger = require('./decomposeToLedger')
 
 module.exports = virtualChannel => {
-  let {
-    id,
-    validity,
-    depositA,
-    depositB,
-    subchanAtoI,
-    subchanBtoI
-  } = virtualChannel
+  let { id, validity, depositA, depositB } = virtualChannel
   const ethcalate = getEthcalate()
-  const web3 = getWeb3()
 
   setTimeout(async () => {
-    const vc = ethcalate.getLatestVirtualStateUpdate(id, ['sigA', 'sigB'])
+    const vc = await ethcalate.getLatestVirtualStateUpdate(id, ['sigA', 'sigB'])
     const { virtualtransactions } = vc
     let finalBalanceA, finalBalanceB
     if (virtualtransactions) {
@@ -29,104 +22,9 @@ module.exports = virtualChannel => {
       finalBalanceB = depositB
     }
 
-    const [
-      { channel: ledgerChannelAtoI },
-      { channel: ledgerChannelBtoI }
-    ] = await Promise.all([
-      ethcalate.getLedgerChannel(subchanAtoI),
-      ethcalate.getLedgerChannel(subchanBtoI)
-    ])
-
-    let ledgerBalanceA, ledgerBalanceIA, ledgerBalanceIB, ledgerBalanceB
-    if (ledgerChannelAtoI.transactions) {
-      const { balanceA, balanceB } = ledgerChannelAtoI.transactions[0]
-      ledgerBalanceA = balanceA
-      ledgerBalanceIA = balanceB
-    } else {
-      ledgerBalanceA = ledgerChannelAtoI.depositA
-      ledgerBalanceIA = ledgerChannelAtoI.depositB
-    }
-
-    if (ledgerChannelBtoI.transactions) {
-      const { balanceA, balanceB } = ledgerChannelBtoI.transactions[0]
-      ledgerBalanceB = balanceA
-      ledgerBalanceIB = balanceB
-    } else {
-      ledgerBalanceB = ledgerChannelBtoI.depositA
-      ledgerBalanceIB = ledgerChannelBtoI.depositB
-    }
-
-    // get BNs
-    ledgerBalanceA = web3.utils.toBN(ledgerBalanceA)
-    ledgerBalanceIA = web3.utils.toBN(ledgerBalanceIA)
-    finalBalanceA = web3.utils.toBN(finalBalanceA)
-    finalBalanceB = web3.utils.toBN(finalBalanceB)
-    depositA = web3.utils.toBN(depositA)
-    depositB = web3.utils.toBN(depositB)
-
-    // decompose into A <=> I ledger channel
-    let amtToTransfer
-    switch (finalBalanceA.cmp(depositA)) {
-      case 0:
-        // balance = deposit, do nothing
-        break
-      case -1:
-        // balance < deposit
-        // ledger balance A = ledgerbalance - (virtualdeposit - virtualbalance)
-        // ledger balance B = ledgerbalance + (virtualdeposit - virtualbalance)
-        amtToTransfer = depositA.sub(finalBalanceA)
-        ledgerBalanceA = ledgerBalanceA.sub(amtToTransfer)
-        ledgerBalanceIA = ledgerBalanceIA.add(amtToTransfer)
-        break
-      case 1:
-        // balance > deposit
-        // ledger balance A = ledgerbalance + (virtualbalance - virtualdeposit)
-        // ledger balance B = ledgerbalance - (virtualbalance - virtualdeposit)
-        amtToTransfer = finalBalanceA.sub(depositA)
-        ledgerBalanceA = ledgerBalanceA.add(amtToTransfer)
-        ledgerBalanceIA = ledgerBalanceIA.sub(amtToTransfer)
-        break
-    }
-
-    // decompose into B <=> I ledger channel
-    switch (finalBalanceB.cmp(depositB)) {
-      case 0:
-        // balance = deposit, do nothing
-        break
-      case -1:
-        // balance < deposit
-        // ledger balance A = ledgerbalance - (virtualdeposit - virtualbalance)
-        // ledger balance B = ledgerbalance + (virtualdeposit - virtualbalance)
-        amtToTransfer = depositB.sub(finalBalanceB)
-        ledgerBalanceB = ledgerBalanceB.sub(amtToTransfer)
-        ledgerBalanceIB = ledgerBalanceIB.add(amtToTransfer)
-        break
-      case 1:
-        // balance > deposit
-        // ledger balance A = ledgerbalance + (virtualbalance - virtualdeposit)
-        // ledger balance B = ledgerbalance - (virtualbalance - virtualdeposit)
-        amtToTransfer = finalBalanceB.sub(depositB)
-        ledgerBalanceB = ledgerBalanceB.sub(amtToTransfer)
-        ledgerBalanceIB = ledgerBalanceIB.add(amtToTransfer)
-        break
-    }
-
-    await ethcalate.updateLedgerState(
-      {
-        channelId: subchanAtoI,
-        balanceA: ledgerBalanceA,
-        balanceB: ledgerBalanceIA
-      },
-      true
-    )
-
-    await ethcalate.updateLedgerState(
-      {
-        channelId: subchanBtoI,
-        balanceA: ledgerBalanceB,
-        balanceB: ledgerBalanceIB
-      },
-      true
-    )
+    await decomposeToLedger(virtualChannel, {
+      virtualBalanceA: finalBalanceA,
+      virtualBalanceB: finalBalanceB
+    })
   }, validity * 1000)
 }
