2c8dd2fef8c0d558cdcab649cf3822b30813dd1c
LayneHaber <layne.haber@gmail.com>
2018-05-22 16:18:47 -0700

    add placeholder for cosign

--- helpers/sendIngridOpeningCerts.js
+++ helpers/sendIngridOpeningCerts.js
@@ -48,28 +48,39 @@ module.exports = async virtualChannel => {
       found[signer] = true
     } else {
       console.log(
-        `Found invalid cert id: ${cert.id}, signer: ${signer}, cert.from: ${
-          cert.from
-        }`
+        `Found invalid cert id: ${cert.id}, signer: ${signer}, cert.from: ${cert.from}`
       )
     }
   })
   if (found[agentA] && found[agentB]) {
-    const certs = await ethcalate.createOpeningCerts(
-      {
-        id,
-        agentA,
-        agentB,
-        ingrid,
-        depositInWei: '0',
-        participantType: 'ingrid'
-      },
-      true
-    )
-    console.log('Ingrid created certs after finding other certs: ', certs)
-    await ethcalate.sendOpeningCerts(id, certs)
-    await ethcalate.updateVirtualChannelStatus({ id, status: 'opened' })
-    return true
+    // has both certs, cosigns opening certs
+    // const cosigned = await ethcalate.cosignOpeningCert({
+    //   certID: cert.id,
+    //   vcID: id
+    // })
+    const cosigned = true // remove once ^^ works
+    if (cosigned) {
+      // I successfully cosigned certificate
+      // generate opening certs
+      const certs = await ethcalate.createOpeningCerts(
+        {
+          id,
+          agentA,
+          agentB,
+          ingrid,
+          depositInWei: '0',
+          participantType: 'ingrid'
+        },
+        true
+      )
+      console.log('Ingrid created certs after finding other certs: ', certs)
+      await ethcalate.sendOpeningCerts(id, certs)
+      await ethcalate.updateVirtualChannelStatus({ id, status: 'opened' })
+      return true
+    } else {
+      console.log('Ingrid did not successfully sign opening certs.')
+      return false
+    }
   } else {
     await ethcalate.checkVcOpeningCerts(virtualChannel)
     return false
