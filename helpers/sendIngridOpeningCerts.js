ed052d8e0f724c84fc10eff50c300fcff89a27a8
Rahul <rksethuram9@gmail.com>
2018-06-01 16:53:52 -0700

    Ingrid fixes

--- helpers/sendIngridOpeningCerts.js
+++ helpers/sendIngridOpeningCerts.js
@@ -34,7 +34,7 @@ module.exports = async virtualChannel => {
     switch (cert.from) {
       case agentA:
         sigParams = {
-          id,
+          virtualChannelId: id,
           agentA,
           agentB,
           ingrid,
@@ -47,7 +47,7 @@ module.exports = async virtualChannel => {
         break
       case agentB:
         sigParams = {
-          id,
+          virtualChannelId: id,
           agentA,
           agentB,
           ingrid,
@@ -61,10 +61,10 @@ module.exports = async virtualChannel => {
     }
     console.log('sigParams: ', sigParams)
     if (sigParams) {
-      const signer = Ethcalate.recoverSignerFromOpeningCerts(
-        cert.sig,
-        sigParams
-      )
+      const signer = Ethcalate.recoverSignerFromOpeningCerts({
+        sig: cert.sig,
+        ...sigParams
+      })
       if (signer === cert.from) {
         found[signer] = true
       } else {
@@ -78,42 +78,46 @@ module.exports = async virtualChannel => {
   })
   if (found[agentA] && found[agentB]) {
     // has both certs, cosigns opening certs
-    try {
-      console.log(typeof id)
-      await ethcalate.cosignOpeningCerts({
-        certA: certs[0].from === agentA ? certs[0].sig : certs[1].sig,
-        certB: certs[0].from === agentA ? certs[1].sig : certs[0].sig,
-        id
-      })
-    } catch (e) {
-      console.log('Ingrid did not successfully sign opening certs.')
-      return false
-    }
+    // try {
+    await ethcalate.cosignOpeningCerts({
+      certA: certs[0].from === agentA ? certs[0].sig : certs[1].sig,
+      certB: certs[0].from === agentA ? certs[1].sig : certs[0].sig,
+      virtualChannelId: id
+    })
+    // } catch (e) {
+    //   console.log('Ingrid did not successfully sign opening certs.')
+    //   return false
+    // }
 
     // I successfully cosigned certificate
     // generate opening certs
-    const ingridCerts = await ethcalate.createOpeningCerts(
-      {
-        id,
-        agentA,
-        agentB,
-        ingrid,
-        depositInWei: '0',
-        participantType: 'ingrid',
-        subchanAtoI,
-        subchanBtoI,
-        closingTimeSeconds
-      },
-      true
-    )
+    const ingridCerts = await ethcalate.createOpeningCerts({
+      virtualChannelId: id,
+      agentA,
+      agentB,
+      ingrid,
+      depositInWei: '0',
+      participantType: 'ingrid',
+      subchanAtoI,
+      subchanBtoI,
+      closingTimeSeconds,
+      unlockedAccountPresent: true
+    })
     console.log(
       'Ingrid created certs after finding and signing other certs: ',
       ingridCerts
     )
-    await ethcalate.sendOpeningCerts(id, ingridCerts)
-    await ethcalate.updateVirtualChannelStatus({ id, status: 'opened' })
+    await ethcalate.sendOpeningCerts({
+      virtualChannelId: id,
+      cert: ingridCerts
+    })
+    await ethcalate.updateVirtualChannelStatus({
+      virtualChannelId: id,
+      status: 'opened'
+    })
     return true
   } else {
+    virtualChannel.virtualChannelId = id
     await ethcalate.checkVcOpeningCerts(virtualChannel)
     return false
   }
